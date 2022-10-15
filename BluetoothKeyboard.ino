589108dc0538b4496ce92d0ffd5700984e7a9bff
oscrodsol <oscar43521@gmail.com>
2022-10-15 13:35:22 +0200

    Added comments to the code

--- BluetoothKeyboard.ino
+++ BluetoothKeyboard.ino
@@ -1,25 +1,28 @@
 
-#include <BleKeyboard.h>
+#include <BleKeyboard.h> //Include the bluetooth keyboard library
 
-#define BUTTON_PIN 21
+#define BUTTON_PIN 21 //Define the pin where is the button conected
 
-BleKeyboard bleKeyboard;
+BleKeyboard bleKeyboard; //Creates an instance of the class blekeyboard
 
-int button_state = 0;
+int button_state = 0; //Initialice the value of the button state
 
 void setup() {
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  Serial.begin(115200);
-  Serial.println("Starting BLE work!");
-  bleKeyboard.begin();
+  pinMode(BUTTON_PIN, INPUT_PULLUP); //Declare the functionality of the pin
+  Serial.begin(115200); //Initialice the serial in the 115200 range
+  Serial.println("Starting BLE work!"); //Prints in the serial monitor
+  bleKeyboard.begin(); //Starts the bluetootk protocol
 }
 
+
+//If the esp is connected, checks if the button is pressed, if the state of the button is low, 
+//the blekeyboard prints whathever we want, in this case an A, then is set a delay to prevent malfunctions in the program
 void loop() {
-  if (bleKeyboard.isConnected()) {
+  if (bleKeyboard.isConnected()) { 
     button_state = digitalRead(BUTTON_PIN);
     if (button_state == LOW) {
       bleKeyboard.print("A");
-      delay(10);
+      delay(100);
     }
   }
 }
